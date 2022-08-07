a02b4a2f35bb03307dc221c4241be0ec5d5ea1d5
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-07 13:55:18 +0600

    Math, abs, pow, round

---
+++ math.js
@@ -0,0 +1,17 @@
+const result = Math.pow(3, 8);
+console.log(result);
+
+const num1 = 25;
+const num2 = 45;
+
+const gap = Math.abs(num1 - num2);
+if (gap < 5) {
+    console.log('Now you are friend');
+}
+else {
+    console.log('We are not friend');
+}
+
+const number = 2.5647;
+const fullNumber = Math.round(number);
+console.log(fullNumber);
