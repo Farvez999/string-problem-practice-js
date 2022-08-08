6e92ca9c68bc2dc728ef0039aa1de71baa48febd
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-08 20:16:26 +0600

    max number with 2&3 Inuput number

---
+++ max.js
@@ -0,0 +1,22 @@
+//2 Input with max
+const jim = 85;
+const dela = 75;
+
+if (jim > dela) {
+    console.log('Jim will get the cake');
+} else {
+    console.log('Dela will get the cake');
+}
+
+//3 Input with max
+const jim3 = 85;
+const dela3 = 75;
+const cinku = 77;
+
+if (jim > dela && jim > cinku) {
+    console.log('Jim will get the cake');
+} else if (dela > jim && dela > cinku) {
+    console.log('Dela will get the cake');
+} else {
+    console.log('Cinku will gat the cake');
+}
